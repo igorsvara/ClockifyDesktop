138c3dce5487a821e0cff15696934cf79022d724
igor <svaraigorw@gmail.com>
2025-03-02 13:58:49 +0100

    initial commit

---
+++ renderer.js
@@ -0,0 +1,10 @@
+document.getElementById('startBtn').addEventListener('click', () => {
+    console.log('Avvio Timer');
+    // In futuro chiamerai l'API di Clockify per avviare il timer
+  });
+  
+  document.getElementById('stopBtn').addEventListener('click', () => {
+    console.log('Ferma Timer');
+    // In futuro chiamerai l'API di Clockify per fermare il timer
+  });
+  
